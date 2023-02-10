3c93ebde3c8dcedd2f47f4e43a59f6dfe3aad1e1
Lucas <lucaslemoine49@gmail.com>
2023-02-10 16:36:15 +0000

    added functionality to set up camera session to capture video and process input and output.

--- Dissertation/Dissertation/ViewController.swift
+++ Dissertation/Dissertation/ViewController.swift
@@ -1,20 +1,90 @@
-//
-//  ViewController.swift
-//  Dissertation
-//
-//  Created by Lucas Lemoine on 10/02/2023.
-//
 
 import UIKit
+import AVFoundation
+import Vision
 
-class ViewController: UIView {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    var bufferSize: CGSize = .zero
+    var rootLayer: CALayer! = nil
+    
+    @IBOutlet weak private var previewView: UIView!
+    private let session = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer! = nil
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    
+    private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAVCapture()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
     }
-    */
 
+    
+    func setupAVCapture() {
+        var deviceInput: AVCaptureDeviceInput!
+        
+        // Select a video device, make an input
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+        } catch {
+            print("Could not create video device input: \(error)")
+            return
+        }
+        
+        session.beginConfiguration()
+        session.sessionPreset = .vga640x480 // Model image size is smaller.
+        
+        // Add a video input
+        guard session.canAddInput(deviceInput) else {
+            print("Couldn't add video device input to the session")
+            session.commitConfiguration()
+            return
+        }
+        session.addInput(deviceInput)
+        if session.canAddOutput(videoDataOutput) {
+            session.addOutput(videoDataOutput)
+            // Add a video data output
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            print("Couldn't add video data output to the session")
+            session.commitConfiguration()
+            return
+        }
+        let captureConnection = videoDataOutput.connection(with: .video)
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        do {
+            try  videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+        session.commitConfiguration()
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        rootLayer = previewView.layer
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+    }
+    
+    func startCaptureSession() {
+        session.startRunning()
+    }
+    
+    // Clean up capture setup
+    func teardownAVCapture() {
+        previewLayer.removeFromSuperlayer()
+        previewLayer = nil
+    }
+    
+    
 }
