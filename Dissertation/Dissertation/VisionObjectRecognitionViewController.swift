ede2dbc8c74e931c94c68fb8f4711b199338c8fc
Lucas <lucaslemoine49@gmail.com>
2023-02-11 17:44:00 +0000

    added functionality to draw the results of object detection in the view.

--- Dissertation/Dissertation/VisionObjectRecognitionViewController.swift
+++ Dissertation/Dissertation/VisionObjectRecognitionViewController.swift
@@ -34,10 +34,80 @@ class VisionObjectRecognitionViewController: ViewController {
         return error
     }
     
-    func drawVisionRequestResults(_ :Any) {
-       
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        detectionOverlay.sublayers = nil // remove all the old recognized objects
+        
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+            
+            
+            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+            
+            let textLayer = self.createTextSubLayerInBounds(objectBounds,
+                                                            identifier: topLabelObservation.identifier,
+                                                            confidence: topLabelObservation.confidence)
+            shapeLayer.addSublayer(textLayer)
+            detectionOverlay.addSublayer(shapeLayer)
+            
+        }
+        self.updateLayerGeometry()
+        CATransaction.commit()
     }
     
-  
+    func setupLayers() {
+         detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+         detectionOverlay.name = "DetectionOverlay"
+         detectionOverlay.bounds = CGRect(x: 0.0,
+                                          y: 0.0,
+                                          width: bufferSize.width,
+                                          height: bufferSize.height)
+         detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+         rootLayer.addSublayer(detectionOverlay)
+         
+     }
+     
+     func updateLayerGeometry() {
+         let bounds = rootLayer.bounds
+         var scale: CGFloat
+         
+         let xScale: CGFloat = bounds.size.width / bufferSize.height
+         let yScale: CGFloat = bounds.size.height / bufferSize.width
+         
+         scale = fmax(xScale, yScale)
+         if scale.isInfinite {
+             scale = 1.0
+         }
+         CATransaction.begin()
+         CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+         
+         // rotate layer into screen orientation, scale, and mirror
+         detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: scale, y: -scale))
+         // center layer
+         detectionOverlay.position = CGPoint(x: bounds.midX, y: bounds.midY)
+         
+         CATransaction.commit()
+         
+     }
+     
+    
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        //TODO
+        return textLayer
+    }
+     
+     func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+         //TODO
+         return shapeLayer
+     }
+
+    
+
 
 }
